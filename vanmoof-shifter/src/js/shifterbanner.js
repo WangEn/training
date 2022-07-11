5d3b958cccc15bc7050d82564ae38a8c8d6ee14a
Warn <imwarn@163.com>
2022-07-11 12:29:08 +0800

    feat:shifter mask尝试进度0.1

--- vanmoof-shifter/src/js/shifterbanner.js
+++ vanmoof-shifter/src/js/shifterbanner.js
@@ -12,17 +12,17 @@ class ShifterBanner {
     this.stage = this.app.stage
 
     this.loader = new PIXI.Loader();
-		this.loader.add('shifter_bike.jpg', './images/shifter_bike.jpg')
-		this.loader.add('shifter_spokes.png', './images/shifter_spokes.png')
-		this.loader.add('shifter_uncovered.png', './images/shifter_uncovered.png')
-		this.loader.add('shifter_covered.png', './images/shifter_covered.png')
-		this.loader.add('shifter_clear.png', './images/shifter_clear.png')
+    this.loader.add('shifter_bike.jpg', './images/shifter_bike.jpg')
+    this.loader.add('shifter_spokes.png', './images/shifter_spokes.png')
+    this.loader.add('shifter_uncovered.png', './images/shifter_uncovered.png')
+    this.loader.add('shifter_covered.png', './images/shifter_covered.png')
+    this.loader.add('shifter_clear.png', './images/shifter_clear.png')
 
 
-		this.loader.load()
+    this.loader.load()
     this.loader.onComplete.add(() => {
-			this.show()
-		})
+      this.show()
+    })
   }
 
   resource (name) {
@@ -32,20 +32,20 @@ class ShifterBanner {
   show () {
     const shifterContainer = new PIXI.Container()
 
-    const shifterBike = this.resource('shifter_bike.jpg')
-    // shifterBike.scale.x = shifterBike.scale.y = 0.6
-    // shifterContainer.addChild(shifterBike)
-
     const shifterSpokes = this.resource('shifter_spokes.png')
     shifterSpokes.scale.x = shifterSpokes.scale.y = 0.6
     shifterSpokes.alpha = 0.3
+
+    const shifterSpokes2 = this.resource('shifter_spokes.png')
+    shifterSpokes2.scale.x = shifterSpokes2.scale.y = 0.6
+
 
     const shifterUncovered = this.resource('shifter_uncovered.png')
     shifterUncovered.scale.x = shifterUncovered.scale.y = 0.6
 
     const shifterCovered = this.resource('shifter_covered.png')
     shifterCovered.scale.x = shifterCovered.scale.y = 0.6
-    
+    shifterUncovered.backgroundAlpha = false
 
     const clearDraw = new PIXI.Graphics()
     clearDraw.beginFill(0xffff00)
@@ -63,30 +63,40 @@ class ShifterBanner {
 
     // const clearDraw = this.resource('shifter_clear.png')
     clearDraw.width = clearDraw.height = 200
-    clearDraw.pivot.x = clearDraw.pivot.y = clearDraw.width/2
+    clearDraw.pivot.x = clearDraw.pivot.y = clearDraw.width / 2
 
+    shifterSpokes2.mask = clearDraw
+    shifterUncovered.mask = clearDraw
+    // shifterSpokes.mask = clearDraw
 
-    shifterCovered.interactive = true
-    shifterCovered.buttonMode = true
-    shifterCovered.on('mousemove', (event) => {
+    shifterSpokes.interactive = true
+    shifterSpokes.buttonMode = true
+    shifterSpokes.on('mousemove', (event) => {
       const { x, y } = event.data.global
       console.log(x, y);
       clearDraw.x = x
       clearDraw.y = y
-      shifterUncovered.mask = clearDraw
+      uncoverFill.alpha=0
+      // clearDraw.mask = shifterUncovered
+      // shifterSpokes.mask = clearDraw
+      // mask is  to alpha=0 where is mask.
+
+      // fillDraw.mask = clearDraw
       // shifterCovered.mask = clearDraw
       // event.backgroundAlpha = 0.2
+      // shifterContainer.mask = clearDraw
     })
 
     shifterContainer.addChild(shifterSpokes)
     shifterContainer.addChild(shifterCovered)
     shifterContainer.addChild(shifterUncovered)
-    shifterContainer.addChild(clearDraw)
+    shifterContainer.addChild(shifterSpokes2)
 
+    shifterContainer.addChild(clearDraw)
 
     this.stage.addChild(shifterContainer)
 
-    function resize() {
+    function resize () {
       // shifterBike.x = window.innerWidth - shifterBike.width
       // shifterBike.y = window.innerHeight - shifterBike.height
       // shifterContainer.x = window.innerWidth - shifterContainer.width
@@ -94,7 +104,7 @@ class ShifterBanner {
 
       // shifterCovered.x = window.innerWidth - shifterBike.width
       // shifterCovered.y = window.innerHeight - shifterBike.height - 600
-      
+
     }
     window.addEventListener('resize', resize)
     resize()
